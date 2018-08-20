bd017bf1f9db8b12db507159871dc7e2b002632b
John Considine <jackconsidine3@gmail.com>
2018-08-20 11:16:23 -0500

    step 5: home component

--- js/app.js
+++ js/app.js
@@ -15,10 +15,17 @@ Vue.component('login-component', {
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus=""> \
       <label for="inputPassword" class="sr-only">Password</label> \
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""> \
-      <button class="btn btn-lg btn-primary btn-block" type="button">Sign in</button> \
+        <button class="btn btn-lg btn-primary btn-block" type="button">Sign in</button> \
       <p class="mt-5 mb-3 text-muted">© 2017-2018</p> \
     </form> \
   </div>'
+})
+
+
+Vue.component("home-component", {
+    template : "<div> \
+    <h1> Hello World! </h1> \
+    </div>"
 })
 
 const app = new Vue({}).$mount('#app')
