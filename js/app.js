e5c1690ce2d9c12d18e1f15b1fc435a35436fbba
John Considine <jackconsidine3@gmail.com>
2018-08-20 11:13:25 -0500

    step 3: vue initialization

--- js/app.js
+++ js/app.js
@@ -5,12 +5,5 @@ Parse.initialize("YOUR_APP_ID", "YOUR_JAVASCRIPT_KEY");
 // 'https://parseapi.back4app.com'
 Parse.serverURL = 'YOUR_SERVER_URL'
 
-
-// If you want to test your parse connection
-new Parse.Object("Test", {"text" : "testing"}).save()
-.then(function (obj) {
-	console.log("Success", obj);
-})
-.catch(function (e) {
-	alert("Error saving test object!" + e.message);
-});
+const app = new Vue({
+}).$mount('#app')
