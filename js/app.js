4d06423b36aab75dcbe2e0d8e7641e7351de8143
John Considine <jackconsidine3@gmail.com>
2018-08-20 11:19:27 -0500

    step 6: vue router integration

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ Parse.initialize("YOUR_APP_ID", "YOUR_JAVASCRIPT_KEY");
 Parse.serverURL = 'YOUR_SERVER_URL'
 
 
-
-Vue.component('login-component', {
+// Assign LoginComponent
+const LoginComponent = Vue.component('login-component', {
     template: '<div class="signin-wrapper text-center"> \
     <form class="form-signin"> \
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1> \
@@ -15,17 +15,28 @@ Vue.component('login-component', {
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus=""> \
       <label for="inputPassword" class="sr-only">Password</label> \
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""> \
-        <button class="btn btn-lg btn-primary btn-block" type="button">Sign in</button> \
+      <button class="btn btn-lg btn-primary btn-block" type="button">Sign in</button> \
       <p class="mt-5 mb-3 text-muted">© 2017-2018</p> \
     </form> \
   </div>'
 })
 
-
-Vue.component("home-component", {
+// Assign HomeComponent
+const HomeComponent = Vue.component("home-component", {
     template : "<div> \
     <h1> Hello World! </h1> \
     </div>"
 })
-
-const app = new Vue({}).$mount('#app')
+// Declare the route mapping
+const routes = [
+  { path: '/', component: HomeComponent },
+  { path: '/login', component: LoginComponent }
+]
+// Initialize the router
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
+// Attach the router to the vue instance
+const app = new Vue({
+  router
+}).$mount('#app')
